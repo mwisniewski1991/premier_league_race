2bab9bc83f93f71ad2f2eab22646f53da381dc27
mwisniewski <mw.data.web@gmail.com>
2023-09-29 18:33:32 +0200

    Create data manager. Script for scrap data and load into DB.

---
+++ data_manager/main.py
@@ -0,0 +1,17 @@
+from fbref_wrangling import pl_race
+from db import db_manager 
+from db.db_manager import DB_context_manager
+
+DB_PATH = 'data_manager/db/pl_race.sqlite'
+
+
+def main() -> None:
+    plrace_data:list[tuple] = pl_race.scrap_plrace_data()
+
+    with DB_context_manager(DB_PATH) as conn:
+        db_manager.create_table(conn)
+        db_manager.delete_all_rows(conn)
+        db_manager.insert_data(conn, plrace_data)
+
+if __name__ == '__main__':
+    main()
