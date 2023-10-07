e6a7637940df3fac7588db17dca70f68fe13424c
mwisniewski <mw.data.web@gmail.com>
2023-10-07 13:00:28 +0200

    Update pl_race scraper

--- app/data_manager/fbref_wrangling/pl_race_scraper.py
+++ app/data_manager/fbref_wrangling/pl_race_scraper.py
@@ -1,20 +1,19 @@
-import pandas as pd
-from fbref_wrangling import wrangling_functions
+from fbref_wrangling import pl_race_wrangling_functions
 
 def scrap_plrace_data() -> list[tuple]:
 
     FBREF_LINK:str = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures"
     
-    pl_table = wrangling_functions.get_table(FBREF_LINK)
+    pl_table = pl_race_wrangling_functions.get_table(FBREF_LINK)
     pl_table = (
     pl_table
-        .pipe(wrangling_functions.dropna, 'Score')
-        .pipe(wrangling_functions.extract_teams_score, 'Score')
-        .pipe(wrangling_functions.extract_teams_points, 'home_score', 'away_score', 'home')
-        .pipe(wrangling_functions.extract_teams_points, 'home_score', 'away_score', 'away')
-        .pipe(wrangling_functions.create_datatime, 'Date', 'Time')
-        .pipe(wrangling_functions.drop_columns, ['Wk', 'Day', 'Venue', 'Referee', 'Match Report', 'Notes', 'Date', 'Time'])
-        .pipe(wrangling_functions.create_race_table)
+        .pipe(pl_race_wrangling_functions.dropna, 'Score')
+        .pipe(pl_race_wrangling_functions.extract_teams_score, 'Score')
+        .pipe(pl_race_wrangling_functions.extract_teams_points, 'home_score', 'away_score', 'home')
+        .pipe(pl_race_wrangling_functions.extract_teams_points, 'home_score', 'away_score', 'away')
+        .pipe(pl_race_wrangling_functions.create_datatime, 'Date', 'Time')
+        .pipe(pl_race_wrangling_functions.drop_columns, ['Wk', 'Day', 'Venue', 'Referee', 'Match Report', 'Notes', 'Date', 'Time'])
+        .pipe(pl_race_wrangling_functions.create_race_table)
     )
 
     return pl_table.to_records(index=False).tolist()
