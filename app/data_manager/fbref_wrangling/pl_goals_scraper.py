94fb17ee7c4e6ac26a9d23542d197e09fdb377fa
mwisniewski <mw.data.web@gmail.com>
2023-10-12 20:23:09 +0200

    Update data_manager

--- app/data_manager/fbref_wrangling/pl_goals_scraper.py
+++ app/data_manager/fbref_wrangling/pl_goals_scraper.py
@@ -1,23 +1,26 @@
 import time
-import pl_goals_wrangling_functions as plgoals_funs
+import pl_goals_scraper_functions as plgoals_funcs
 
 
-def scrap_goals_data() -> list:
+def scrap_goals_data() -> list[tuple]:
 
     LINK = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures"
 
-    matchreports_links = plgoals_funs.get_all_matchreport_links(LINK)
+    matchreports_links = plgoals_funcs.get_all_matchreport_links(LINK)
 
     match_goals_list = []
-    for report_link in matchreports_links:
+    for report_link in matchreports_links[:4]:
         print(report_link)
-        soup = plgoals_funs.get_soup(report_link)
-        match_scorers = plgoals_funs.get_match_scorers(soup)
-        match_date = plgoals_funs.get_match_date(soup)
-        match_goals = plgoals_funs.create_goal_object(match_scorers, match_date) 
+        soup = plgoals_funcs.get_soup(report_link)
+        match_scorers = plgoals_funcs.get_match_scorers(soup)
+        match_date = plgoals_funcs.get_match_date(soup)
+        match_goals = plgoals_funcs.create_goal_object(match_scorers, match_date) 
 
         match_goals_list.extend(match_goals)
         time.sleep(3)
+    
+
+
 
     return match_goals_list
 
