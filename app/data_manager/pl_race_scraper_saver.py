ac32cd8838830c5678d453c9c92e2339ea4ac306
mwisniewski <mw.data.web@gmail.com>
2023-10-27 20:28:03 +0200

    Update app

--- app/data_manager/pl_race_scraper_saver.py
+++ app/data_manager/pl_race_scraper_saver.py
@@ -2,7 +2,7 @@ from fbref_wrangling import pl_race_scraper
 from db import db_manager 
 from db.db_manager import DB_context_manager
 
-DB_PATH = 'app/data_manager/db/pl_race.sqlite'
+DB_PATH = 'data_manager/db/pl_race.sqlite'
 
 def main() -> None:
     plrace_data:list[tuple] = pl_race_scraper.scrap_plrace_data()
--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from data_manager.db.db_manager import DB_context_manager
 import altair as alt
 
-DB_PATH:str = 'app/data_manager/db/pl_race.sqlite'
+DB_PATH:str = 'data_manager/db/pl_race.sqlite'
 SQL_SELECT:str = 'SELECT * FROM teams_race;'
 
 with DB_context_manager(DB_PATH) as conn:
@@ -11,6 +11,10 @@ with DB_context_manager(DB_PATH) as conn:
 
 st.set_page_config(page_title="Premier League Race", layout="wide")
 st.title("Premier League Race")
+
+domain:list[str] = ['Arsenal','Aston Villa','Bournemouth','Brentford','Brighton','Burnley','Chelsea','Crystal Palace','Everton','Fulham','Liverpool','Luton Town','Manchester City','Manchester Utd','Newcastle Utd',"Nott'ham Forest",'Sheffield Utd','Tottenham','West Ham','Wolves']
+
+range_:list[str] = ['#EF0107','#95bfe5','#c8102E','#CC0000','#0057B8','#6C1D45','#034694','#1B458F','#003399','#CC0000','#c8102E','orange','#6CABDD','#DA291C','#FFFFFF',"#DD0000",'#DA291C','#FFFFFF','#7A263A','#FDB913',]
 
 
 with st.sidebar:
@@ -28,7 +32,13 @@ pl_racechart = (
         .encode(
                 x = alt.Y('match_order', title='Match', scale=alt.Scale(domain=[1,36])),
                 y = alt.Y('points_cum', title='Points', scale=alt.Scale(domain=[0,100])),
-                color='team_name')
+                color=alt.Color(
+                    'team_name', 
+                    legend=alt.Legend(legendY=3, orient='bottom', direction='horizontal', titleAnchor='middle', title='Team name'),
+                    scale=alt.Scale(domain=domain, range=range_)
+                    )
+                
+                )
   
 )
 
